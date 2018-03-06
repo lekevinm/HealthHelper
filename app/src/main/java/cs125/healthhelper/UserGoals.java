54161d78478795e70f257735a0c8a5198843b84e
jacquetd <jacquetd@users.noreply.github.com>
2018-03-05 19:14:57 -0800

    Added UserGoals class

---
+++ app/src/main/java/cs125/healthhelper/UserGoals.java
@@ -0,0 +1,28 @@
+package cs125.healthhelper;
+
+/**
+ * Created by Jacqueline on 3/4/2018.
+ */
+
+public class UserGoals {
+
+    public int calorieGoal;
+    public int fatGoal;
+    public int carbGoal;
+    public int proteinGoal;
+
+    public UserGoals()
+    {
+
+    }
+
+    public UserGoals(int calorieGoal, int fatGoal, int carbGoal, int proteinGoal)
+    {
+        this.calorieGoal = calorieGoal;
+        this.fatGoal = fatGoal;
+        this.carbGoal = carbGoal;
+        this.proteinGoal = proteinGoal;
+    }
+
+
+}
